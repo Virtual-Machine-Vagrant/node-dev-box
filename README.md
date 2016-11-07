b935627087eab5b7b48de07186aee093e442f332
Sergey Kanukov <s.kanukov@gmail.com>
2016-11-07 19:23:33 +0300

    Install soft via bootstrap script and add some helpers

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # node-dev-box
 A Vagrant powered virtual machine for NodeJS application development.
+
+## Requirements
+
+* [VirtualBox](https://www.virtualbox.org)
+
+* [Vagrant](http://vagrantup.com)
+
+## How To Build The Virtual Machine
+
+Building the virtual machine is this easy:
+
+    host $ git clone https://github.com/skanukov/node-dev-box
+    host $ cd node-dev-box
+    host $ vagrant up
+
+That's it.
+
+After the installation has finished, you can access the virtual machine with
+
+    host $ vagrant ssh
+
+Default PostgreSQL port 5432 in the host computer is forwarded to port 5432 in the virtual machine.
+
+Don't forget to look at some helper shell scripts for newbies.
+
+## What's In The Box
+
+* Ubuntu 16.04
+
+* PostgreSQL 9.6
+
+* Git 2.7
+
+* NodeJS v6.9 with Npm 3.10
+
+## License
+
+Released under the MIT License, Copyright (c) 2016 Sergey Kanukov, inspired by official [Rails dev box](https://github.com/rails/rails-dev-box).
---
+++ bootstrap.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+# Bootstrap file for setting NodeJS development environment.
+
+postgresql_version='9.6'
+
+# Heper functions
+function append_to_file {
+  echo "$1" | sudo tee -a "$2"
+}
+
+function replace_in_file {
+  sudo sed -i "$1" "$2"
+}
+
+function install {
+  echo "Installing $1..."
+  shift
+  sudo apt-get -y install "$@"
+}
+
+function update_packages {
+  echo 'Updating package information...'
+  sudo apt-get -y update
+}
+# End of Heper functions
+
+function install_dependencies {
+  # Install required dependencies here
+}
+
+# PostgreSQL
+function install_postgresql {
+  append_to_file \
+    'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' \
+    /etc/apt/sources.list.d/pgdg.list
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+    sudo apt-key add -
+  update_packages
+
+  install 'PostgreSQL' postgresql-"$postgresql_version" libpq-dev
+}
+
+function create_vagrant_superuser {
+  sudo -u postgres createuser -s vagrant
+}
+
+function allow_external_connections {
+  append_to_file \
+    'host all all all password' \
+    /etc/postgresql/"$postgresql_version"/main/pg_hba.conf
+  replace_in_file \
+    "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" \
+    /etc/postgresql/"$postgresql_version"/main/postgresql.conf
+}
+
+function install_postgresql_and_allow_external_connections {
+  install_postgresql
+  create_vagrant_superuser
+  allow_external_connections
+}
+# End of PostgreSQL
+
+# NodeJS
+function install_node {
+  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  install 'NodeJS' nodejs
+}
+
+function set_npm_permissions {
+  echo 'Setting correct Npm permissions...'
+  mkdir ~/.npm-global
+  npm config set prefix '~/.npm-global'
+  append_to_file 'export PATH=~/.npm-global/bin:$PATH' ~/.profile
+  source ~/.profile
+}
+
+function install_node_and_npm {
+  install_node
+  set_npm_permissions
+}
+# End of NodeJS
+
+
+update_packages
+install_dependencies
+install_postgresql_and_allow_external_connections
+install_node_and_npm
+
+echo 'All set, rock on!'
